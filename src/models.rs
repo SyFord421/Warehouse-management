90df71aa93aed4f47a1f31098499dfe94d6a7d50
SyFord421 <jaki05118@gmail.com>
2026-04-29 10:47:55 +0700

    feat: now using Hashmap for faster search

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use colored::*;
 use serde_json;
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug,)]
 //pub biar bisa di gunakan dari luar
@@ -18,17 +19,16 @@ impl Item {
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Inventory {
-    pub items: Vec<Item>,
+    pub items: HashMap<String, Item>,
 }
 
 impl Inventory {
     pub fn new() -> Self {
         Self {
-            items:Vec::new(),
+            items:HashMap::new(),
         }
     }
-    //Method yang Fungsi-nya untuk mengkonversi vector dan menyimpannya di dalam file json
-    pub fn save_to_file(&self, filename: &str) {//mengubah vector jadi string
+    pub fn save_to_file(&self, filename: &str) {
         match serde_json::to_string_pretty(&self.items) {
                 Ok(json) => {
                     if let Err(e) = std::fs::write(filename, json){//menuliskan kedalam file dan langsung check jika gagal
@@ -43,14 +43,14 @@ impl Inventory {
     }
     //Method untuk memasukkan Struct kedalam Vector
     pub fn add_item(&mut self, item: Item){//move smantic memindahkan kepemilikan
-        self.items.push(item);
+        self.items.insert(item.name.clone(), item);
         println!("{} Berhasil Menambahkan Barang baru", "[✓]".green());
     }
     //method untuk memuat data yang tersimpan di json
     pub fn load_from_file(filename: &str) -> Self{
     //mencheck apakah file ada jika tidak berikan vector kosong
         if let Ok(content) = std::fs::read_to_string(filename){
-            match serde_json::from_str::<Vec<Item>>(&content){
+            match serde_json::from_str::<HashMap<String, Item>>(&content){
                 Ok(items) => {
                     println!(
                     "{} Berhasil Memuat data", "[✓]".green());
@@ -64,25 +64,18 @@ impl Inventory {
         Self::new()
     }
     
-    pub fn show_all_item(&self){//pinjam isi Vector
-        for item in &self.items{
-            println!(
-        "Name: {} | Price: {} | Stock: {} 
-        ",
-        item.name,
-        item.price,
-        item.stock);
+    pub fn show_all_item(&self) {
+    // Di HashMap, kita dapet kuncinya (nama) dan isinya (item)
+    for (name, item) in &self.items {
+        println!("Name: {} | Price: {} | Stock: {}", name, item.price, item.stock);
         }
     }
+
     
     
     //Method Untuk mencari item menggunakan nama
     pub fn find_item(&self, name: &str){
-        let found = self
-        .items
-        .iter()
-        .find(|i| i.name.to_lowercase() == name.to_lowercase());
-        if let Some(item) = found {
+        if let Some(item) = self.items.get(name) {
             println!(
             "Name: {} | Price: {} | Stock: {}",
             item.name,
@@ -95,64 +88,49 @@ impl Inventory {
     }
     
     pub fn calculate_all(&self){
-        let total_value: f64 = self.items.iter().map(|item| {
-            let total = item.price * item.stock as f64;
-            println!("Nama: {} | Stock: {} Total: {}", item.name, item.stock, total);
-            total
-        }).sum();
-        println!("Total {}", total_value);
+    let total_value: f64 = self.items.values().map(|item| {
+        let total = item.price * item.stock as f64;
+        println!("Nama: {} | Stock: {} Subtotal: {}", item.name, item.stock, total);
+        total
+    }).sum();
+    println!("{} Total Nilai Value: {}", ">>".yellow(), total_value);
     }
-    //Method untuk menghapus item dari vector dengan cara menyimpan Hanya data yang tidak mirip dengan yang di cari
+    
     pub fn remove_item(&mut self, name: &str){
-        let initial_len = self.items.len();
-        self.items
-        .retain(|item|
-        item.name.to_lowercase() != name.to_lowercase());
-        if self.items.len() < initial_len {
-            println!("{} Barang Berhasil Di Keluarkan", "[✓]".green());
+        if self.items.remove(name).is_some(){
+            println!("{} Barang Berhasil Di hapus", "[✓]".green());
         }else{
-            println!("{} Barang Tidak di temukan", "[×]".red());
+            println!("{} Barang Tidak Di Temukan", "[×]".red());
         }
     }
     
     pub fn update_or_add(&mut self, name: String, price: f64, add_stock: u32){
-        let item_found = self.items
-        .iter_mut()
-        .find(|i| i.name.to_lowercase() == name.to_lowercase());
-        match item_found {
-            Some(item) => {
-                item.stock += add_stock;
-                println!(
-                "{} Stok {} ditambah {}!  Total jadi: {}",
-                "Update:".blue(),
-                item.name,
-                add_stock,
-                item.stock);
-            }, None => {
-                self.items.push(Item::new(name, price, add_stock));
-                println!("{} Item berhasil di tambahkan", "[✓]".green());
-            }
+        if let Some(item) = self.items.get_mut(&name) {
+            item.stock += add_stock;
+            println!("{} Stok {} ditambah {}!", "Update:".blue(), item.name, add_stock);
+        }else{
+            let new_item = Item::new(name.clone(), price, add_stock);
+            self.items.insert(name, new_item);
+            println!("{} Barang Telah Ditambahkan", "[✓]".green());
         }
     }
     
     pub fn sell_item(&mut self, name: &str, amount: u32){
-        let item_found = self.items.iter_mut().find(|i| i.name.to_lowercase() == name.to_lowercase());
-        match item_found {
+        match self.items.get_mut(name){
             Some(item) => {
-                if item.stock >= amount {
+                if item.stock >= amount{
                     item.stock -= amount;
                     println!("{} Berhasil menjual {} {}. Sisa stok: {}", "Success:".green(), amount, item.name, item.stock);
-                    println!("TOTAL: {}", item.price * amount as f64);
                 }else{
-                    println!("{} Stok cuma ada {}, nggak cukup buat jual {}", "[×]".red(), item.stock, amount);
+                    println!("{} Stok hanya ada {}, Tidak Cukup!", "[×]".red(), item.stock);
                 }
-            }, None => println!("{} Barang Tidak di temukan", "[×]".red()),
+            },
+            None => println!("{} Barang tidak ditemukan!", "[×]".red()),
         }
     }
     
     pub fn check_low_stock(&self, limit: u32){
-        let low_items: Vec<&Item> =
-        self.items.iter().filter(|i| i.stock <= limit).collect();
+        let low_items: Vec<&Item> = self.items.values().filter(|i| i.stock <= limit).collect();
         if low_items.is_empty(){
             println!("{} Stock Masih Aman ", "[✓]".green());
         }else{
